b3daa7b3d3caae8e9a7a650462e6927c6562b314
Magdalena Kubincova <sleepydesertfox@tutanota.com>
2021-10-20 16:36:12 +0200

    Added error handling and styling

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const https = require('https');
 const bodyParser = require("body-parser");
+const { runInNewContext } = require('vm');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
 
 //get html page with form
 app.get("/", function(req, res) {
@@ -18,27 +20,39 @@ app.post("/", function(req, res) {
     const url = "https://restcountries.com/v2/name/" + query;
 
     https.get(url, function(response){
-        console.log(response.statusCode);
-
+    
         //take data from api
         response.on("data", function(data){
 
+            
             //make data readable
             const countryData = JSON.parse(data);
-            const name = countryData[0].name;
-            const capital = countryData[0].capital;
-            const flag = countryData[0].flags.svg;
 
-            //show result to user
-            res.write("<a href='/'>&larr; Go back</a>");
-            res.write("<h1>" + name + "</h1>");
-            res.write("<h2>Capital: " + capital + "</h2>");
-            res.write("<img src=" + flag + " alt=" + name + "-flag />");
+            res.write("<html><head><link rel='stylesheet' href='css/style.css'></head>");
+            res.write("<a href='/'>&larr; Go back</a><br>");
 
-            res.send();
+            if (countryData.status == 404) {
+                res.write("<p>No country with this name, try again</p>");
+                res.send();
+            } else{
+                const name = countryData[0].name;
+                const capital = countryData[0].capital;
+                const population = countryData[0].population;
+                const currency = countryData[0].currencies[0].name;
+                const flag = countryData[0].flags.svg;
+
+                //show result to user
+                res.write("<h1><img src=" + flag + " alt=" + name + "-flag /> " + name + "</h1>");
+                res.write("<p>Capital: <span>" + capital + "</span></p>");
+                res.write("<p>Population: <span>" + population + "</span></p>");
+                res.write("<p>Currency: <span>" + currency + "</span></p>");
+                res.write("</html>")
+
+                res.send();
+            }
+
         });
-    });     
-
+    })
 })
 
 
